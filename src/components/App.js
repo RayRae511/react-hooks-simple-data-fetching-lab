401f91259f78dec9f70b6b68c21caaa44cef4b3c
Raymond Oeta <oetaray@gmail.com>
2023-08-03 13:17:57 +0300

    completed work

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useEffect } from 'react'
+
+const App = () => {
+    const [doggo, setDoggo] = useEffect(null)
+    const [loading, setLoading] = useEffect(false)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((r) => r.json())
+        .then((data)=> {
+            setDoggo(data.message)
+            setLoading(true)
+        })
+    }, [])
+  return (
+    <div>
+        {loading ? (
+            <img src={doggo}/>
+        ) : (<p>Loading...</p>)}
+    </div>
+  )
+}
+
+export default App 
